75b575c8428dbfdeafa76ed5ac235559a07d16b5
mrplcardoso <mrplcardoso@gmail.com>
2014-05-10 16:31:14 -0300

    feito um testezinho de atacante, onde ele corre em direção a bola, em linha reta, e depois chuta

---
+++ Assets/_Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour 
+{
+	public float m_impulse, m_desaceleracao, m_velocity;
+	public Vector3 m_direction;
+
+	void Awake()
+	{
+		m_impulse = 0.0f;
+		m_desaceleracao = 0.1f;
+		m_velocity = 0.0f;
+		m_direction = Vector3.zero;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		m_velocity = m_impulse;
+		transform.position = new Vector3(transform.position.x + (m_velocity * m_direction.x * Time.deltaTime),
+													transform.position.y,
+													transform.position.z + (m_velocity * m_direction.z * Time.deltaTime));
+		m_impulse -= m_desaceleracao;
+		if (m_impulse <= 0.001)
+			m_impulse = 0.0f;
+	}
+}
---
+++ Assets/_Scripts/Forward.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forward : MonoBehaviour 
+{
+	public GameObject ball;
+	public float m_velocity, m_shootforce, m_angle;
+	public Vector3 m_direction;
+
+	void Awake()
+	{
+		ball = GameObject.Find("Bola");
+		m_velocity = 2.0f;
+		m_shootforce = 5.0f;
+		m_direction = Vector3.zero;
+		m_angle = 0.0f;
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		m_direction = ball.transform.position - transform.position;
+		transform.position = new Vector3(transform.position.x + (m_velocity * m_direction.x * Time.deltaTime),
+																			transform.position.y,
+																			transform.position.z + (m_velocity * m_direction.z * Time.deltaTime));
+		//Shoot();
+	}
+
+	void Shoot()
+	{
+		if(transform.position.x >=10.0f)
+		{
+			m_shootforce = 15.0f;
+			m_angle = Random.RandomRange(-0.5f, 0.5f);
+			m_velocity = 0.0f;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.name == "Bola")
+		{
+			if (transform.position.x >= 10.0f)
+			{
+				m_shootforce = 15.0f;
+				m_angle = Random.RandomRange(-0.5f, 0.5f);
+				m_velocity = 0.0f;
+			}
+			col.GetComponent<Ball>().m_impulse = m_shootforce;
+			col.GetComponent<Ball>().m_direction.x = Mathf.Cos(m_angle);
+			col.GetComponent<Ball>().m_direction.z = Mathf.Sin(m_angle);
+			Debug.Log(col.GetComponent<Ball>().m_direction);
+			//Debug.Log("Colidiu");
+		}
+	}
+}
