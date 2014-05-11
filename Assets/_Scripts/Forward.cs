4505e0b7b6a179de901befff632193dc441a4d32
mrplcardoso <mrplcardoso@gmail.com>
2014-05-10 23:28:28 -0300

    Feito atacante conduzir a bola ate a grande área e chutar.

--- Assets/_Scripts/Forward.cs
+++ Assets/_Scripts/Forward.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class Forward : MonoBehaviour 
 {
-	public GameObject ball;
+	public GameObject ball, gol;
 	public float m_velocity, m_shootforce, m_angle;
-	public Vector3 m_direction;
+	public Vector3 m_direction, ball_direction;
 
 	void Awake()
 	{
 		ball = GameObject.Find("Bola");
+		gol = GameObject.Find("TraveDir");
 		m_velocity = 2.0f;
 		m_shootforce = 5.0f;
-		m_direction = Vector3.zero;
+		m_direction = ball_direction = Vector3.zero;
 		m_angle = 0.0f;
 	}
 	// Use this for initialization
@@ -28,34 +29,38 @@ public class Forward : MonoBehaviour
 		transform.position = new Vector3(transform.position.x + (m_velocity * m_direction.x * Time.deltaTime),
 																			transform.position.y,
 																			transform.position.z + (m_velocity * m_direction.z * Time.deltaTime));
-		//Shoot();
+		Debug.Log(Vector3.Distance(ball.transform.position, transform.position));
 	}
 
-	void Shoot()
+	void Shoot(Collider col)
 	{
-		if(transform.position.x >=10.0f)
-		{
-			m_shootforce = 15.0f;
-			m_angle = Random.RandomRange(-0.5f, 0.5f);
-			m_velocity = 0.0f;
-		}
+		m_shootforce = 15.0f;
+		m_angle = Random.Range(-0.3f, 0.3f);
+		col.GetComponent<Ball>().m_direction.x = Mathf.Cos(m_angle);
+		col.GetComponent<Ball>().m_direction.z = Mathf.Sin(m_angle);
+		m_velocity = 0.0f;
+	}
+
+	void MoveForward (Collider col)
+	{
+		ball_direction = (gol.transform.position - ball.transform.position).normalized;
+		col.GetComponent<Ball>().m_direction.x = ball_direction.x;
+		col.GetComponent<Ball>().m_direction.z = ball_direction.z;
 	}
 
 	void OnTriggerEnter(Collider col)
 	{
-		if(col.name == "Bola")
+		if(col.name == ball.name)
 		{
 			if (transform.position.x >= 10.0f)
 			{
-				m_shootforce = 15.0f;
-				m_angle = Random.RandomRange(-0.5f, 0.5f);
-				m_velocity = 0.0f;
+				Shoot(col);
+			}
+			else// if (transform.position.x < 10.0f)
+			{
+				MoveForward(col);
 			}
 			col.GetComponent<Ball>().m_impulse = m_shootforce;
-			col.GetComponent<Ball>().m_direction.x = Mathf.Cos(m_angle);
-			col.GetComponent<Ball>().m_direction.z = Mathf.Sin(m_angle);
-			Debug.Log(col.GetComponent<Ball>().m_direction);
-			//Debug.Log("Colidiu");
 		}
 	}
 }
