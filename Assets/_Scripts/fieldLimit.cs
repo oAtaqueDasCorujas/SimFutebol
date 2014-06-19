c311458adfa3b1afa85d84a2723f61b1cc62eb18
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2014-06-19 17:25:14 -0300

    Tiro de meta implementado e corrigido a trave..

--- Assets/_Scripts/fieldLimit.cs
+++ Assets/_Scripts/fieldLimit.cs
@@ -9,12 +9,18 @@ public class fieldLimit : MonoBehaviour {
     {
         direction = other.GetComponent<Ball>().mvec3_direction;
 
-        if (gameObject.name == "parede")
+        if (gameObject.name == "parede") {
             direction = new Vector3(direction.x * -1, direction.y, direction.z);
+            other.GetComponent<Ball>().mf_impulse = 10;
+            if (other.transform.position.x > 0)
+                other.transform.position = new Vector3 (16, ((other.transform.position.y < 0)? 2 : -2), 0);
+            else
+                other.transform.position = new Vector3 (-16, ((other.transform.position.y < 0)? 2 : -2), 0);
+        }            
         else if (gameObject.name == "paredeLateral")
             direction = new Vector3(direction.x, direction.y * -1, direction.z);
         else
-            direction *= 1;
+            direction *= -1;
 
         other.GetComponent<Ball>().mvec3_direction = direction;
     }
