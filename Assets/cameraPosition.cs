3afd533825bd8db4c10b5d16a54d3c631d614510
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2014-06-19 16:43:20 -0300

    Criação de posição variadas para camera

---
+++ Assets/cameraPosition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraPosition : MonoBehaviour {
+        
+    public GameObject camera;
+    public Transform[] posCamera;
+    public int index;
+
+	void Update () {
+
+        Choice();
+
+        if (index > 6)
+        {
+            camera.transform.position = posCamera[index].position;
+        }
+	
+	}
+
+    void Choice() {
+
+        if (Input.inputString.Length > 0) {
+		
+			string keyPressedString = Input.inputString.Substring
+									(Input.inputString.Length - 1, 1);
+			Debug.Log(keyPressedString);
+
+            index = int.Parse(keyPressedString);
+
+            if (index >= 0 && index <= 9)
+            {
+                camera.transform.position = posCamera[index].position;
+                camera.transform.rotation = posCamera[index].rotation;
+            }			
+		}
+    }
+}
