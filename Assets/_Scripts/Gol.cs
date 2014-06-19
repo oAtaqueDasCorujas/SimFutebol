bdef4cbe93d90724078780be0f7af28cff99337b
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2014-06-18 22:52:58 -0300

    Ajustes de limite de campo e trigger do gol

---
+++ Assets/_Scripts/Gol.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gol : MonoBehaviour {
+
+	// Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Bola")
+            Application.LoadLevel(Application.loadedLevel);
+        
+    }
+}
---
+++ Assets/_Scripts/fieldLimit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fieldLimit : MonoBehaviour {
+
+    Vector3 direction;
+
+    void OnTriggerEnter(Collider other)
+    {
+        direction = other.GetComponent<Ball>().mvec3_direction;
+
+        if (gameObject.name == "parede")
+            direction = new Vector3(direction.x * -1, direction.y, direction.z);
+        else if (gameObject.name == "paredeLateral")
+            direction = new Vector3(direction.x, direction.y * -1, direction.z);
+        else
+            direction *= 1;
+
+        other.GetComponent<Ball>().mvec3_direction = direction;
+    }
+}
